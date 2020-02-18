0dfbee7a0fccdd76e0f17c797adf77a1f00400e9
yjh0410 <1394571815@qq.com>
2020-02-18 18:07:59 +0800

    add darknet-53 model

--- backbone/darknet.py
+++ backbone/darknet.py
@@ -7,13 +7,13 @@ import os
 import sys
 
 
-__all__ = ['darknet19']
+__all__ = ['darknet19', 'darknet53']
 
 class Conv_BN_LeakyReLU(nn.Module):
-    def __init__(self, in_channels, out_channels, ksize, padding=0, dilation=1):
+    def __init__(self, in_channels, out_channels, ksize, padding=0, stride=1, dilation=1):
         super(Conv_BN_LeakyReLU, self).__init__()
         self.convs = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, ksize, padding=padding, dilation=dilation),
+            nn.Conv2d(in_channels, out_channels, ksize, padding=padding, stride=stride, dilation=dilation),
             nn.BatchNorm2d(out_channels),
             nn.LeakyReLU(0.1, inplace=True)
         )
@@ -22,23 +22,19 @@ class Conv_BN_LeakyReLU(nn.Module):
         return self.convs(x)
 
 class resblock(nn.Module):
-    def __init__(self, ch, nblocks=1, shortcut=True):
-
+    def __init__(self, ch, nblocks=1):
         super().__init__()
-        self.shortcut = shortcut
         self.module_list = nn.ModuleList()
-        for i in range(nblocks):
-            resblock_one = nn.ModuleList()
-            resblock_one.append(add_conv(ch, ch//2, 1, 1))
-            resblock_one.append(add_conv(ch//2, ch, 3, 1))
+        for _ in range(nblocks):
+            resblock_one = nn.Sequential(
+                Conv_BN_LeakyReLU(ch, ch//2, 1),
+                Conv_BN_LeakyReLU(ch//2, ch, 3, padding=1)
+            )
             self.module_list.append(resblock_one)
 
     def forward(self, x):
         for module in self.module_list:
-            h = x
-            for res in module:
-                h = res(h)
-            x = x + h if self.shortcut else h
+            x = module(x) + x
         return x
 
 class DarkNet_19(nn.Module):
@@ -111,6 +107,57 @@ class DarkNet_19(nn.Module):
         # return x
         return C_4, C_5, C_6
 
+class DarkNet_53(nn.Module):
+    """
+    YOLOv3 model module. The module list is defined by create_yolov3_modules function. \
+    The network returns loss values from three YOLO layers during training \
+    and detection results during test.
+    """
+    def __init__(self, num_classes=1000):
+        super(DarkNet_53, self).__init__()
+        # stride = 2
+        self.layer_1 = nn.Sequential(
+            Conv_BN_LeakyReLU(3, 32, 3, padding=1),
+            Conv_BN_LeakyReLU(32, 64, 3, padding=1, stride=2),
+            resblock(64, nblocks=1)
+        )
+        # stride = 4
+        self.layer_2 = nn.Sequential(
+            Conv_BN_LeakyReLU(64, 128, 3, padding=1, stride=2),
+            resblock(128, nblocks=2)
+        )
+        # stride = 8
+        self.layer_3 = nn.Sequential(
+            Conv_BN_LeakyReLU(128, 256, 3, padding=1, stride=2),
+            resblock(256, nblocks=8)
+        )
+        # stride = 16
+        self.layer_4 = nn.Sequential(
+            Conv_BN_LeakyReLU(256, 512, 3, padding=1, stride=2),
+            resblock(512, nblocks=8)
+        )
+        # stride = 32
+        self.layer_5 = nn.Sequential(
+            Conv_BN_LeakyReLU(512, 1024, 3, padding=1, stride=2),
+            resblock(1024, nblocks=4)
+        )
+
+        # self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        # self.fc = nn.Linear(1024, num_classes)
+
+    def forward(self, x, targets=None):
+        x = self.layer_1(x)
+        x = self.layer_2(x)
+        C_3 = self.layer_3(x)
+        C_4 = self.layer_4(C_3)
+        C_5 = self.layer_5(C_4)
+
+        # x = self.avgpool(x)
+        # x = x.view(x.size(0), -1)
+        # x = self.fc(x)
+
+        return C_3, C_4, C_5
+
 def darknet19(pretrained=False, hr=False, **kwargs):
     """Constructs a darknet-19 model.
 
@@ -123,10 +170,26 @@ def darknet19(pretrained=False, hr=False, **kwargs):
         path_to_dir = os.path.dirname(os.path.abspath(__file__))
         if hr:
             print('Loading the hi-res darknet19-448 ...')
-            # If you have more GPUs, you should delete the 'map_location'.
-            model.load_state_dict(torch.load(path_to_dir + '/weights/darknet19_72.96.pth', map_location='cuda'), strict=False)
+            model.load_state_dict(torch.load(path_to_dir + '/weights/darknet19_hr_75.52_92.73.pth', map_location='cuda'), strict=False)
         else:
             print('Loading the darknet19 ...')
-            # If you have more GPUs, you should delete the 'map_location'.
             model.load_state_dict(torch.load(path_to_dir + '/weights/darknet19_72.96.pth', map_location='cuda'), strict=False)
+    return model
+
+def darknet53(pretrained=False, hr=False, **kwargs):
+    """Constructs a darknet-53 model.
+
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    model = DarkNet_53()
+    if pretrained:
+        print('Loading the pretrained model ...')
+        path_to_dir = os.path.dirname(os.path.abspath(__file__))
+        if hr:
+            print('Loading the hi-res darknet53-448 ...')
+            model.load_state_dict(torch.load(path_to_dir + '/weights/darknet53_hr_77.76.pth', map_location='cuda'), strict=False)
+        else:
+            print('Loading the darknet53 ...')
+            model.load_state_dict(torch.load(path_to_dir + '/weights/darknet53_75.42.pth', map_location='cuda'), strict=False)
     return model
