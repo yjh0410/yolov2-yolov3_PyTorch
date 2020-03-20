632a6ddf09c4442b021e6f7d4695577c89410251
Yonghye Kwon <developer.0hye@gmail.com>
2020-03-20 22:22:59 +0900

    Cleanup & IMPROVE mAP +0.1% !!!
    original version
    yolov3 tiny voc
    AP for aeroplane = 0.7445
    AP for bicycle = 0.8010
    AP for bird = 0.6944
    AP for boat = 0.6162
    AP for bottle = 0.5641
    AP for bus = 0.7888
    AP for car = 0.8225
    AP for cat = 0.7794
    AP for chair = 0.5475
    AP for cow = 0.7662
    AP for diningtable = 0.6554
    AP for dog = 0.7303
    AP for horse = 0.7880
    AP for motorbike = 0.8114
    AP for person = 0.7820
    AP for pottedplant = 0.4380
    AP for sheep = 0.7135
    AP for sofa = 0.6836
    AP for train = 0.7983
    AP for tvmonitor = 0.7302
    Mean AP = 0.7128
    
    This Modified Version
    
    AP for aeroplane = 0.7479
    AP for bicycle = 0.8012
    AP for bird = 0.6936
    AP for boat = 0.6154
    AP for bottle = 0.5640
    AP for bus = 0.7900
    AP for car = 0.8230
    AP for cat = 0.7842
    AP for chair = 0.5478
    AP for cow = 0.7675
    AP for diningtable = 0.6597
    AP for dog = 0.7297
    AP for horse = 0.7885
    AP for motorbike = 0.8131
    AP for person = 0.7825
    AP for pottedplant = 0.4372
    AP for sheep = 0.7142
    AP for sofa = 0.6847
    AP for train = 0.7995
    AP for tvmonitor = 0.7301
    Mean AP = 0.7137
    
    71.28% VS 71.37

--- models/tiny_yolo_v3.py
+++ models/tiny_yolo_v3.py
@@ -1,16 +1,15 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.autograd import Variable
-from torchvision import models
-from utils import Conv2d, reorg_layer
+import torchvision
+from utils import Conv2d
 from backbone import *
-import os
 import numpy as np
 
-class myYOLOv3(nn.Module):
-    def __init__(self, device, input_size=None, num_classes=20, trainable=False, conf_thresh=0.01, nms_thresh=0.50, anchor_size=None, hr=False):
-        super(myYOLOv3, self).__init__()
+class YOLOv3tiny(nn.Module):
+    def __init__(self, device, input_size=None, num_classes=20, trainable=False, conf_thresh=0.01, nms_thresh=0.50,
+                 anchor_size=None, hr=False):
+        super(YOLOv3tiny, self).__init__()
         self.device = device
         self.num_classes = num_classes
         self.trainable = trainable
@@ -18,18 +17,21 @@ class myYOLOv3(nn.Module):
         self.nms_thresh = nms_thresh
         self.stride = [8, 16, 32]
         self.anchor_size = torch.tensor(anchor_size).view(3, len(anchor_size) // 3, 2)
+        self.all_anchors_wh = self.anchor_size.view(3, 1, 1, 1, 3, 2).to(device)
         self.anchor_number = self.anchor_size.size(1)
         self.anchor_size[0, :] *= 4
         self.anchor_size[1, :] *= 2
         self.anchor_size[2, :] *= 1
         self.input_size = input_size
-        self.grid_cell, self.all_anchor_wh, self.stride_tensor = self.init_grid()
+
+        w, h = self.input_size
+        self.grids_cell = [self.create_grid(w // s, h // s, self.device) for s in self.stride]
         self.scale = np.array([[input_size[1], input_size[0], input_size[1], input_size[0]]])
         self.scale_torch = torch.tensor(self.scale.copy()).float()
 
         # backbone darknet-19
         self.backbone = darknet_tiny(pretrained=trainable, hr=hr)
-        
+
         # s = 32
         self.conv_set_3 = nn.Sequential(
             Conv2d(512, 256, 1, leakyReLU=True),
@@ -40,7 +42,7 @@ class myYOLOv3(nn.Module):
         )
         self.conv_1x1_3 = Conv2d(256, 128, 1, leakyReLU=True)
         self.extra_conv_3 = Conv2d(256, 512, 3, padding=1, leakyReLU=True)
-        self.pred_3 = nn.Conv2d(512, self.anchor_number*(1 + 4 + self.num_classes), 1)
+        self.pred_3 = nn.Conv2d(512, self.anchor_number * (1 + 4 + self.num_classes), 1)
 
         # s = 16
         self.conv_set_2 = nn.Sequential(
@@ -52,7 +54,7 @@ class myYOLOv3(nn.Module):
         )
         self.conv_1x1_2 = Conv2d(128, 64, 1, leakyReLU=True)
         self.extra_conv_2 = Conv2d(128, 256, 3, padding=1, leakyReLU=True)
-        self.pred_2 = nn.Conv2d(256, self.anchor_number*(1 + 4 + self.num_classes), 1)
+        self.pred_2 = nn.Conv2d(256, self.anchor_number * (1 + 4 + self.num_classes), 1)
 
         # s = 8
         self.conv_set_1 = nn.Sequential(
@@ -63,55 +65,13 @@ class myYOLOv3(nn.Module):
             Conv2d(128, 64, 1, leakyReLU=True),
         )
         self.extra_conv_1 = Conv2d(64, 128, 3, padding=1, leakyReLU=True)
-        self.pred_1 = nn.Conv2d(128, self.anchor_number*(1 + 4 + self.num_classes), 1)
-    
-    def init_grid(self):
-        s = self.stride
-        total = sum([(self.input_size[1]//s) * (self.input_size[0]//s) for s in self.stride])
-        # [1, H*W, 1, 2]
-        grid_cell = torch.zeros(1, total, self.anchor_number, 2).to(self.device)
-        # [1, 1, anchor_n, 2]
-        all_anchor_wh = torch.zeros(1, total, self.anchor_number, 2).to(self.device)
+        self.pred_1 = nn.Conv2d(128, self.anchor_number * (1 + 4 + self.num_classes), 1)
 
-        stride_tensor = torch.zeros(1, total, self.anchor_number).to(self.device).float()
-
-        start_index = 0
-        for i, s in enumerate(self.stride):
-            ws = self.input_size[1] // s
-            hs = self.input_size[0] // s
-            for ys in range(hs):
-                for xs in range(ws):
-                    index = ys * ws + xs + start_index
-                    grid_cell[:, index, :, :] = torch.tensor([xs, ys]).float()
-                    stride_tensor[:, index, :] = torch.ones(self.anchor_number) * s
-            all_anchor_wh[:, start_index:index] = self.anchor_size[i]
-            start_index += ws * hs
-
-        return grid_cell, all_anchor_wh, stride_tensor.view(1, -1)
-        
-    def decode_boxes(self, xywh_pred):
-        """
-            Input:
-                xywh_pred : [B, H*W, anchor_n, 4] containing [tx, ty, tw, th]
-            Output:
-                bbox_pred : [B, H*W, anchor_n, 4] containing [c_x, c_y, w, h]
-        """
-        # b_x = sigmoid(tx) + gride_x,  b_y = sigmoid(ty) + gride_y
-        B, HW, ab_n, _ = xywh_pred.size()
-        c_xy_pred = torch.sigmoid(xywh_pred[:, :, :, :2]) + self.grid_cell
-        # b_w = anchor_w * exp(tw),     b_h = anchor_h * exp(th)
-        b_wh_pred = torch.exp(xywh_pred[:, :, :, 2:]) * self.all_anchor_wh
-        # [B, H*W, anchor_n, 4] -> [B, H*W*anchor_n, 4]
-        bbox_pred = torch.cat([c_xy_pred, b_wh_pred], -1).view(B, HW*ab_n, 4)
-
-        # [center_x, center_y, w, h] -> [xmin, ymin, xmax, ymax]
-        output = torch.zeros(bbox_pred.size())
-        output[:, :, 0] = (bbox_pred[:, :, 0] - bbox_pred[:, :, 2] / 2) * self.stride_tensor
-        output[:, :, 1] = (bbox_pred[:, :, 1] - bbox_pred[:, :, 3] / 2) * self.stride_tensor
-        output[:, :, 2] = (bbox_pred[:, :, 0] + bbox_pred[:, :, 2] / 2) * self.stride_tensor
-        output[:, :, 3] = (bbox_pred[:, :, 1] + bbox_pred[:, :, 3] / 2) * self.stride_tensor
-        
-        return output
+    def create_grid(self, w, h, device):
+        grid_y, grid_x = torch.meshgrid([torch.arange(h), torch.arange(w)])
+        grid_xy = torch.stack([grid_x, grid_y], dim=-1).to(device).float()
+        grid_xy = grid_xy.view(1, h, w, 1, 2)
+        return grid_xy
 
     def clip_boxes(self, boxes, im_shape):
         """
@@ -119,31 +79,24 @@ class myYOLOv3(nn.Module):
         """
         if boxes.shape[0] == 0:
             return boxes
-
-        # x1 >= 0
-        boxes[:, 0::4] = np.maximum(np.minimum(boxes[:, 0::4], im_shape[1] - 1), 0)
-        # y1 >= 0
-        boxes[:, 1::4] = np.maximum(np.minimum(boxes[:, 1::4], im_shape[0] - 1), 0)
-        # x2 < im_shape[1]
-        boxes[:, 2::4] = np.maximum(np.minimum(boxes[:, 2::4], im_shape[1] - 1), 0)
-        # y2 < im_shape[0]
-        boxes[:, 3::4] = np.maximum(np.minimum(boxes[:, 3::4], im_shape[0] - 1), 0)
+        boxes[:, [0, 2]] = np.clip(boxes[:, [0, 2]], 0., 1.)
+        boxes[:, [1, 3]] = np.clip(boxes[:, [1, 3]], 0., 1.)
         return boxes
 
     def nms(self, dets, scores):
         """"Pure Python NMS baseline."""
-        x1 = dets[:, 0]  #xmin
-        y1 = dets[:, 1]  #ymin
-        x2 = dets[:, 2]  #xmax
-        y2 = dets[:, 3]  #ymax
+        x1 = dets[:, 0]  # xmin
+        y1 = dets[:, 1]  # ymin
+        x2 = dets[:, 2]  # xmax
+        y2 = dets[:, 3]  # ymax
 
-        areas = (x2 - x1) * (y2 - y1)                 # the size of bbox
-        order = scores.argsort()[::-1]                        # sort bounding boxes by decreasing order
+        areas = (x2 - x1) * (y2 - y1)  # the size of bbox
+        order = scores.argsort()[::-1]  # sort bounding boxes by decreasing order
 
-        keep = []                                             # store the final bounding boxes
+        keep = []  # store the final bounding boxes
         while order.size > 0:
-            i = order[0]                                      #the index of the bbox with highest confidence
-            keep.append(i)                                    #save it to keep
+            i = order[0]  # the index of the bbox with highest confidence
+            keep.append(i)  # save it to keep
             xx1 = np.maximum(x1[i], x1[order[1:]])
             yy1 = np.maximum(y1[i], y1[order[1:]])
             xx2 = np.minimum(x2[i], x2[order[1:]])
@@ -155,24 +108,21 @@ class myYOLOv3(nn.Module):
 
             # Cross Area / (bbox + particular area - Cross Area)
             ovr = inter / (areas[i] + areas[order[1:]] - inter)
-            #reserve all the boundingbox whose ovr less than thresh
+            # reserve all the boundingbox whose ovr less than thresh
             inds = np.where(ovr <= self.nms_thresh)[0]
             order = order[inds + 1]
 
         return keep
 
-    def postprocess(self, all_local, all_conf, exchange=True, im_shape=None):
+    def postprocess(self, all_local, all_conf, all_cls, im_shape=None):
         """
         bbox_pred: (HxW*anchor_n, 4), bsize = 1
         prob_pred: (HxW*anchor_n, num_classes), bsize = 1
         """
         bbox_pred = all_local
-        prob_pred = all_conf
+        scores = all_conf
+        cls_inds = all_cls
 
-        cls_inds = np.argmax(prob_pred, axis=1)
-        prob_pred = prob_pred[(np.arange(prob_pred.shape[0]), cls_inds)]
-        scores = prob_pred.copy()
-        
         # threshold
         keep = np.where(scores >= self.conf_thresh)
         bbox_pred = bbox_pred[keep]
@@ -238,52 +188,72 @@ class myYOLOv3(nn.Module):
         total_obj_pred = []
         total_cls_pred = []
         total_xywh_pred = []
+        total_xyxy_pred = []
         B = HW = 0
-        for pred in preds:
+        for pred, grid_cell, anchors_wh, stride in zip(preds, self.grids_cell, self.all_anchors_wh, self.stride):
             B_, abC_, H_, W_ = pred.size()
-
             # [B, anchor_n * C, H, W] -> [B, H, W, anchor_n * C] -> [B, H*W, anchor_n*C]
-            pred = pred.permute(0, 2, 3, 1).contiguous().view(B_, H_*W_, abC_)
+            # pred = pred.view
+            # print(pred.shape)
+            # exit(1)
 
-            # Divide prediction to obj_pred, xywh_pred and cls_pred   
+            pred = pred.permute(0, 2, 3, 1).contiguous().view(B_, H_ * W_, abC_)
+
+            # Divide prediction to obj_pred, xywh_pred and cls_pred
             # [B, H*W*anchor_n, 1]
-            obj_pred = pred[:, :, :1 * self.anchor_number].contiguous().view(B_, H_*W_*self.anchor_number, 1)
+            obj_pred = pred[:, :, :1 * self.anchor_number].contiguous().view(B_, H_ * W_ * self.anchor_number, 1)
             # [B, H*W*anchor_n, num_cls]
-            cls_pred = pred[:, :, 1 * self.anchor_number : (1 + self.num_classes) * self.anchor_number].contiguous().view(B_, H_*W_*self.anchor_number, self.num_classes)
+            cls_pred = pred[:, :,
+                       1 * self.anchor_number: (1 + self.num_classes) * self.anchor_number].contiguous().view(B_,
+                                                                                                              H_ * W_ * self.anchor_number,
+                                                                                                              self.num_classes)
             # [B, H*W*anchor_n, 4]
             xywh_pred = pred[:, :, (1 + self.num_classes) * self.anchor_number:].contiguous()
+
+            if not self.trainable:
+                obj_pred = torch.sigmoid(obj_pred)
+                cls_pred = torch.softmax(cls_pred, dim=2)
+
+                xywh_pred = xywh_pred.view(B_, H_, W_, 3, 4)
+                xywh_pred[..., :2] = torch.sigmoid(xywh_pred[..., :2])+grid_cell
+                xywh_pred[..., 2:] = torch.exp(xywh_pred[..., 2:])*anchors_wh
+
+                xyxy_pred = torch.zeros_like(xywh_pred)
+                xyxy_pred[..., 0] = (xywh_pred[..., 0] - xywh_pred[..., 2] / 2) * stride
+                xyxy_pred[..., 1] = (xywh_pred[..., 1] - xywh_pred[..., 3] / 2) * stride
+                xyxy_pred[..., 2] = (xywh_pred[..., 0] + xywh_pred[..., 2] / 2) * stride
+                xyxy_pred[..., 3] = (xywh_pred[..., 1] + xywh_pred[..., 3] / 2) * stride
+                xyxy_pred /= self.scale_torch
+                xyxy_pred = torch.clamp(xyxy_pred, 0., 1.)
+                xyxy_pred = xyxy_pred.view(B_, H_*W_*3, 4)
+                total_xyxy_pred.append(xyxy_pred)
+
+            xywh_pred = xywh_pred.view(B_, H_ * W_ * 3, 4)
 
             total_obj_pred.append(obj_pred)
             total_cls_pred.append(cls_pred)
             total_xywh_pred.append(xywh_pred)
             B = B_
-            HW += H_*W_
-        
+            HW += H_ * W_
+
         obj_pred = torch.cat(total_obj_pred, 1)
         cls_pred = torch.cat(total_cls_pred, 1)
         xywh_pred = torch.cat(total_xywh_pred, 1)
 
         # test
         if not self.trainable:
-            xywh_pred = xywh_pred.view(B, HW, self.anchor_number, 4)
+            xyxy_pred = torch.cat(total_xyxy_pred, 1)
             with torch.no_grad():
-                # batch size = 1                
-                all_obj = torch.sigmoid(obj_pred)[0]           # 0 is because that these is only 1 batch.
-                all_bbox = self.decode_boxes(xywh_pred)[0] / self.scale_torch
-                all_class = (torch.softmax(cls_pred[0, :, :], 1) * all_obj)
-                # separate box pred and class conf
-                all_obj = all_obj.to('cpu').numpy()
-                all_class = all_class.to('cpu').numpy()
+                # batch size = 1.
+                all_bbox = xyxy_pred[0]
+                all_class_conf, all_cls_idx = torch.max(obj_pred[0]*cls_pred[0], dim=1)
+
                 all_bbox = all_bbox.to('cpu').numpy()
+                all_class_conf = all_class_conf.to('cpu').numpy()
+                all_cls_idx = all_cls_idx.to('cpu').numpy()
 
-                bboxes, scores, cls_inds = self.postprocess(all_bbox, all_class)
-                # clip the boxes
-                bboxes *= self.scale
-                bboxes = self.clip_boxes(bboxes, self.input_size) / self.scale
-
-    
-                # print(len(all_boxes))
-                return bboxes, scores, cls_inds
+                bboxes, scores, classes = self.postprocess(all_bbox, all_class_conf, all_cls_idx)
+                return bboxes, scores, classes
 
         xywh_pred = xywh_pred.view(B, -1, 4)
         final_prediction = torch.cat([obj_pred, cls_pred, xywh_pred], -1)
