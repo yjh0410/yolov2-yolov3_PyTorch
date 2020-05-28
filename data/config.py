14a7f8d6a95294f1759458ae7f404e3d632f52b2
yjh0410 <1394571815@qq.com>
2020-05-28 17:01:54 +0800

    update

--- data/config.py
+++ data/config.py
@@ -1,13 +1,10 @@
 # config.py
 import os.path
 
-# for making bounding boxes pretty
-COLORS = ((255, 0, 0, 128), (0, 255, 0, 128), (0, 0, 255, 128),
-          (0, 255, 255, 128), (255, 0, 255, 128), (255, 255, 0, 128))
-MEANS = (104, 117, 123)
-# ANCHOR SIZE is from the anchor config of YOLO-v2. But, following is searched by myself!!!
-ANCHOR_SIZE = [[1.19, 1.98], [2.79, 4.59], [4.53, 8.92], [8.06, 5.29], [10.32, 10.65]] 
-      
+
+# single level anchor box config for VOC and COCO
+ANCHOR_SIZE = [[1.19, 1.98], [2.79, 4.59], [4.53, 8.92], [8.06, 5.29], [10.32, 10.65]]
+
 ANCHOR_SIZE_COCO = [[0.53, 0.79], [1.71, 2.36], [2.89, 6.44], [6.33, 3.79], [9.03, 9.74]]
 
 # multi level anchor box config for VOC and COCO
@@ -19,20 +16,17 @@ MULTI_ANCHOR_SIZE_COCO = [[12.48, 19.2], [31.36, 46.4],[46.4, 113.92],
                           [97.28, 55.04], [133.12, 127.36], [79.04, 224.],
                           [301.12, 150.4 ], [172.16, 285.76], [348.16, 341.12]]
 
+
+
+
+
 IGNORE_THRESH = 0.5
 
-
-# yolo-v2 config
 voc_ab = {
     'num_classes': 20,
-    'lr_epoch': (150, 200, 250), # (60, 90, 160),
+    'lr_epoch': (150, 200), # (60, 90, 160),
     'max_epoch': 250,
     'min_dim': [416, 416],
-    'ms_channels':[128, 256, 512],
-    'stride': 32,
-    'strides': [16, 32],
-    'multi_scale': [[320, 320], [352, 352], [384, 384], [416, 416], [448, 448],
-                 [480, 480], [512, 512], [544, 544], [576, 576], [608, 608]],
     'variance': [0.1, 0.2],
     'clip': True,
     'name': 'VOC',
@@ -40,14 +34,9 @@ voc_ab = {
 
 coco_ab = {
     'num_classes': 80,
-    'lr_epoch': (80, 120, 160),
-    'max_epoch': 160,
+    'lr_epoch': (150, 200), # (60, 90, 160),
+    'max_epoch': 260,
     'min_dim': [416, 416],
-    'ms_channels':[128, 256, 512],
-    'stride': 32,
-    'strides': [16, 32],
-    'multi_scale': [[320, 320], [352, 352], [384, 384], [416, 416], [448, 448],
-                 [480, 480], [512, 512], [544, 544], [576, 576], [608, 608]],
     'variance': [0.1, 0.2],
     'clip': True,
     'name': 'COCO',
